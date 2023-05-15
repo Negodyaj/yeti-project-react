a5b1eb16100e3e2138927887db09d6d703c6417b
Anton Efremenkov <anton.efremenkov@itentika.ru>
2023-05-15 16:33:58 +0300

    react-hook-form added

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -1,25 +1,82 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import * as yup from 'yup';
+import { Controller, useForm } from 'react-hook-form';
+import { CustomSelect } from '../../shared/CustomSelect/CustomSelect';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+const schema = yup
+  .object({
+    firstName: yup.string().required(),
+    lastName: yup.string().required(),
+    email: yup.string().required().email(),
+    gender: yup.object({
+      value: yup.number().required('Please select a gender'),
+    }),
+    password: yup.string().required().min(6),
+    repeatPassword: yup
+      .string()
+      .required('Please repeat your password')
+      .oneOf([yup.ref('password')], 'Passwords must match'),
+  })
+  .required();
+type FormData = yup.InferType<typeof schema>;
+
 export const RegisterPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    control,
+  } = useForm<FormData>({
+    resolver: yupResolver(schema),
+  });
+
+  const genderOptions = [
+    { value: 1, label: 'Male' },
+    { value: 2, label: 'Female' },
+    { value: 3, label: 'Other' },
+  ];
+
+  // api call will be here (later)
+  const onSubmit = (data: FormData) => console.log(data);
+
   return (
-    <>
+    <div className="container">
       <h1>Be a part of our life!</h1>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label>
-          First name: <input type="text" />
+          First name: <input type="text" {...register('firstName')} />
         </label>
+        <p className="validation-error">{errors.firstName?.message}</p>
         <label>
-          Last name: <input type="text" />
+          Last name: <input type="text" {...register('lastName')} />
         </label>
+        <p className="validation-error">{errors.lastName?.message}</p>
         <label>
-          Email: <input type="email" />
+          Email: <input type="email" {...register('email')} />
         </label>
+        <p className="validation-error">{errors.email?.message}</p>
         <label>
-          Password: <input type="password" />
+          Gender:
+          <Controller
+            name="gender"
+            control={control}
+            render={({ field }) => (
+              <CustomSelect options={genderOptions} placeholder="Select your gender" field={field} />
+            )}
+          />
         </label>
+        <p className="validation-error">{errors.gender?.message}</p>
         <label>
-          Repeat password: <input type="password" />
+          Password: <input type="password" {...register('password')} />
         </label>
+        <p className="validation-error">{errors.password?.message}</p>
+        <label>
+          Repeat password: <input type="password" {...register('repeatPassword')} />
+        </label>
+        <p className="validation-error">{errors.repeatPassword?.message}</p>
         <button type="submit">Register</button>
       </form>
-    </>
+    </div>
   );
 };
--- src/shared/CustomSelect/CustomSelect.tsx
+++ src/shared/CustomSelect/CustomSelect.tsx
@@ -1,65 +1,58 @@
-import React from "react";
-import Select, { StylesConfig } from "react-select";
+import React from 'react';
+import Select, { StylesConfig } from 'react-select';
 
 interface CustomSelectProps {
   options: {
     value: number;
     label: string;
   }[];
+  placeholder?: string;
+  field?: any;
 }
 
 export const CustomSelect = (props: CustomSelectProps) => {
-  const { options } = props;
+  const { options, placeholder, field } = props;
   const customStyles: StylesConfig = {
     control: (styles, state: any) => ({
       ...styles,
-      backgroundColor: "white",
+      backgroundColor: 'white',
       borderRadius: 0,
-      border: state.isFocused ? "1px solid #ff8b67" : "1px solid #cccccc",
-      boxShadow: state.isFocused ? "0px 0px 1px #ff8b67" : "none",
-      "&:hover": {
-        border: "1px solid #ff8b67",
-        boxShadow: "0px 0px 1px #ff8b67",
+      border: state.isFocused ? '1px solid #ff8b67' : '1px solid #cccccc',
+      boxShadow: state.isFocused ? '0px 0px 1px #ff8b67' : 'none',
+      '&:hover': {
+        border: '1px solid #ff8b67',
+        boxShadow: '0px 0px 1px #ff8b67',
       },
     }),
     option: (styles, { isDisabled, isFocused, isSelected }) => {
       return {
         ...styles,
-        backgroundColor: isDisabled
-          ? undefined
-          : isSelected
-          ? "red"
-          : isFocused
-          ? "purple"
-          : undefined,
-        color: isDisabled ? "#ccc" : isSelected ? "white" : "black",
-        cursor: isDisabled ? "not-allowed" : "default",
+        backgroundColor: isDisabled ? undefined : isSelected ? 'red' : isFocused ? 'purple' : undefined,
+        color: isDisabled ? '#ccc' : isSelected ? 'white' : 'black',
+        cursor: isDisabled ? 'not-allowed' : 'default',
 
-        ":active": {
-          ...styles[":active"],
-          backgroundColor: !isDisabled
-            ? isSelected
-              ? "green"
-              : "blue"
-            : undefined,
+        ':active': {
+          ...styles[':active'],
+          backgroundColor: !isDisabled ? (isSelected ? 'green' : 'blue') : undefined,
         },
       };
     },
     placeholder: (styles) => ({ ...styles }),
     clearIndicator: (styles) => ({
       ...styles,
-      color: "red",
-      "&:hover": {
-        color: "darkred",
+      color: 'red',
+      '&:hover': {
+        color: 'darkred',
       },
     }),
   };
 
   return (
     <Select
+      {...field}
       options={options}
       isClearable
-      placeholder="Выберите размер"
+      placeholder={placeholder ?? 'Choose anything'}
       styles={customStyles}
     />
   );
